ac4aec9c99fb66bbfb0dffb87c6c128c829d0807
Neeraj722 <neerajsinha002@gmail.com>
2018-03-05 00:56:50 +0530

    First Commit

--- src/main/java/applicationPages/LoginPage.java
+++ src/main/java/applicationPages/LoginPage.java
@@ -1,10 +1,24 @@
 package applicationPages;
 
+import net.serenitybdd.core.pages.PageObject;
+import org.openqa.selenium.WebDriver;
+
+import java.util.HashMap;
+
 /**
  * Hello world!
  *
  */
-public class LoginPage
+public class LoginPage extends PageObject
 {
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public static HashMap<String, String> pageMap;
+    static{
+        pageMap= new HashMap<String, String>();
+        pageMap.put("", "");
+    }
 
 }
