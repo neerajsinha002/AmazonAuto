3aa6eb6356f33626ba29b7b4ce298b7c465e9c6c
Neeraj722 <neerajsinha002@gmail.com>
2018-03-05 01:06:52 +0530

    First Commit

---
+++ src/main/java/steps/EndUserSteps.java
@@ -0,0 +1,9 @@
+package steps;
+
+import applicationPages.LoginPage;
+import net.thucydides.core.steps.ScenarioSteps;
+
+public class EndUserSteps extends ScenarioSteps{
+
+    LoginPage loginPage;
+}
