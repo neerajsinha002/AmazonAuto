6f0bd3b65b457918d720ce319d8f095b20587845
Neeraj722 <neerajsinha002@gmail.com>
2018-03-07 00:00:19 +0530

    First Commit

--- src/test/java/cukes/Runner.java
+++ src/test/java/cukes/Runner.java
@@ -1,25 +1,28 @@
 package cukes;
 
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
 import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
 import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
 import org.testng.annotations.BeforeSuite;
 
 /**
  * Unit test for simple LoginPage.
  */
 
-//@RunWith(ExtendedCucumber.class)
+@RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(jsonReport = "target/cucumber.json",
-        retryCount = 3,
-        detailedReport = true,
-        detailedAggregatedReport = true,
+        retryCount = 0,
+        //detailedReport = true,
+        //detailedAggregatedReport = true,
         overviewReport = true,
         //coverageReport = true,
-        jsonUsageReport = "target/cucumber-usage.json",
-        usageReport = true,
-        toPDF = true,
-        excludeCoverageTags = {"@flaky" },
-        includeCoverageTags = {"@passed" },
+        //jsonUsageReport = "target/cucumber-usage.json",
+       // usageReport = true,
+        //toPDF = true,
+        //excludeCoverageTags = {"@flaky" },
+        //includeCoverageTags = {"@passed" },
+        //ReportPrefix = {""},
         outputFolder = "target")
 @CucumberOptions(plugin = { "html:target/cucumber-html-report",
         "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
@@ -27,8 +30,7 @@ import org.testng.annotations.BeforeSuite;
         features = { "./src/test/java/com/github/mkolisnyk/cucumber/features" },
         glue = { "com/github/mkolisnyk/cucumber/steps" },
         tags = {"@consistent"})
-
-public class Runner{
+class Runner{
     @BeforeSuite
     public void launchURL(){
 
