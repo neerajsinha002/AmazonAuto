3a47f3d45b5ed5e6631f436cbdf3bf7b8c6f7144
Neeraj722 <neerajsinha002@gmail.com>
2018-03-11 22:16:27 +0530

    Updated EndUserSteps

---
+++ src/main/java/applicationPages/HomePage.java
@@ -0,0 +1,4 @@
+package applicationPages;
+
+public class HomePage {
+}
--- src/main/java/steps/EndUserSteps.java
+++ src/main/java/steps/EndUserSteps.java
@@ -1,9 +1,17 @@
 package steps;
 
+import applicationPages.HomePage;
 import applicationPages.LoginPage;
+import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 
 public class EndUserSteps extends ScenarioSteps{
 
     LoginPage loginPage;
+    HomePage homePage;
+
+    @Step
+    public void callButton(){
+
+    }
 }
